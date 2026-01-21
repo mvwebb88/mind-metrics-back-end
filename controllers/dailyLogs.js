b3aa0e01edc1506eb79a74da4582aacdd40dab8c
Thais Palma de Brito <britop.tech@gmail.com>
2026-01-21 12:00:01 -0800

    feat(dailyLogs): add POST route to create a daily log

--- controllers/dailyLogs.js
+++ controllers/dailyLogs.js
@@ -3,9 +3,18 @@ const verifyToken = require("../middleware/verify-token.js");
 const DailyLog = require("../models/dailyLog.js");
 const router = express.Router();
 
-
-// add routes here
-
+// POST /dailylogs
+router.post("/", verifyToken, async (req, res) => {
+    try {
+        req.body.userId = req.user._id;
+        const dailyLog = await DailyLog.create(req.body);
+        // .doc is used to populate the userId
+        dailyLog._doc.userId = req.user;
+        res.status(201).json(dailyLog);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
 
 
 
